b786cfdbf10c90a45d92a40d8f416b0ecd570cdb
traydenpoke <traybouc@hotmail.com>
2022-11-21 06:42:22 -0500

    first commit

--- employee.h
+++ employee.h
@@ -1,9 +1,10 @@
 #include <iostream>
 
-struct Employee {
-	Employee(int id, std::string name);
-	Employee();
-	int id;
-	std::string name;
-	void setId(int id); // needed for when an employee leaves company
+class Employee {
+	public:
+		Employee(int id, std::string name);
+		Employee();
+		int id;
+		std::string name;
+		void setId(int id); // needed for when an employee leaves company
 };
